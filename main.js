3c1e4af488d2dd61e02e492165d25aeffbbc985b
Nick Borisenko <nborisenko55@gmail.com>
2022-04-22 16:10:45 -0500

    span color class, moved menu svg to css, mediaq for mobile nav, open close works

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-const mobileNav = document.querySelector('.mobile_nav');
+const mobileNavToggle = document.querySelector('.mobile_nav_toggle');
+const primaryNav = document.querySelector('.primary_navigation');
 const mobileScrollNav = document.querySelector('.mobile_scroll_navigation');
+const projectsSection = document.querySelector('#projects');
+const aboutSection = document.querySelector('#about');
+const contactSection = document.querySelector('#contact');
 
 // Scroll Effects
 window.onscroll = function () {
@@ -10,8 +14,10 @@ window.onscroll = function () {
 function showMobileNav() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mobileScrollNav.style.display = 'block';
+    primaryNav.style.display = 'none';
   } else {
     mobileScrollNav.style.display = 'none';
+    primaryNav.style.display = 'block';
   }
 }
 
@@ -19,27 +25,35 @@ function showMobileNav() {
 // Scrolls to top
 function scrollToTop() {
   document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.documentElement.scrollTo({ top: 0, behavior: 'smooth' }); // For Chrome, Firefox, IE and Opera
 }
 
 // Scrolls to portfolio
 function scrollToPortfolio() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop; // For Safari
+  projectsSection.scrollIntoView({ behavior: 'smooth' }); // For Chrome, Firefox, IE and Opera
 }
 
 // Scrolls to about
 function scrollToAbout() {
   document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  aboutSection.scrollIntoView({ behavior: 'smooth' }); // For Chrome, Firefox, IE and Opera
 }
 
 // Scrolls to contact
 function scrollToContact() {
   document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  contactSection.scrollIntoView({ behavior: 'smooth' }); // For Chrome, Firefox, IE and Opera
 }
 
-mobileNav.addEventListener('click', function () {
-  console.log('Click');
+mobileNavToggle.addEventListener('click', function () {
+  const visibility = primaryNav.getAttribute('data-visible');
+
+  if (visibility === 'false') {
+    primaryNav.setAttribute('data-visible', true);
+    mobileNavToggle.setAttribute('aria-expanded', true);
+  } else {
+    primaryNav.setAttribute('data-visible', false);
+    mobileNavToggle.setAttribute('aria-expanded', false);
+  }
 });
