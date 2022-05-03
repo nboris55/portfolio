6eaa5e96b12fb97183e0faa89fa887c912f718d2
Nick Borisenko <nborisenko55@gmail.com>
2022-05-03 09:16:00 -0500

    ease in headline

--- main.js
+++ main.js
@@ -4,10 +4,59 @@ const projectsSection = document.querySelector('#projects');
 const aboutSection = document.querySelector('#about');
 const contactSection = document.querySelector('#contact');
 
-//Facts vs Hobbies Toggle
+// Hello animation
+const animatedHeadline = document.querySelector('#animated-headline');
+const animatedStr = animatedHeadline.textContent;
+const splitHeadline = animatedStr.split('');
+animatedHeadline.textContent = '';
+let numb = 1;
+for (i = 0; i < splitHeadline.length; i++) {
+  animatedHeadline.innerHTML += `<span style="--i:${numb}">` + splitHeadline[i] + '</span>';
+  numb++;
+}
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick() {
+  const span = animatedHeadline.querySelectorAll('span')[char];
+  span.classList.add('animated_headline');
+  char++;
+  if (char === splitHeadline.length) {
+    complete();
+    return;
+  }
+}
+
+function complete() {
+  clearInterval(timer);
+  timer = null;
+}
+
+// Facts vs Hobbies Toggle
 const aboutToggle = document.querySelector('#about-toggle');
 const facts = document.querySelector('#facts');
 const hobbies = document.querySelector('#hobbies');
+
+//Show and hide Facts list vs Hobbies list
+aboutToggle.addEventListener('click', () => {
+  const visibility = document.getElementById('facts').getAttribute('data-visible');
+
+  if (visibility === 'false') {
+    document.getElementById('facts').setAttribute('data-visible', true);
+    document.getElementById('facts').setAttribute('aria-expanded', true);
+    document.getElementById('hobbies').setAttribute('data-visible', false);
+    document.getElementById('hobbies').setAttribute('aria-expanded', false);
+    facts.style.display = 'block';
+    hobbies.style.display = 'none';
+  } else {
+    document.getElementById('hobbies').setAttribute('data-visible', true);
+    document.getElementById('hobbies').setAttribute('aria-expanded', true);
+    document.getElementById('facts').setAttribute('data-visible', false);
+    document.getElementById('facts').setAttribute('aria-expanded', false);
+    facts.style.display = 'none';
+    hobbies.style.display = 'block';
+  }
+});
 
 // Scroll Effects
 window.onscroll = function () {
@@ -39,6 +88,7 @@ function scrollToContact() {
   contactSection.scrollIntoView({ behavior: 'smooth' }); // For Chrome, Firefox, IE and Opera
 }
 
+// Mobile menu toggle
 mobileNavToggle.addEventListener('click', function () {
   const visibility = primaryNav.getAttribute('data-visible');
 
@@ -48,26 +98,5 @@ mobileNavToggle.addEventListener('click', function () {
   } else {
     primaryNav.setAttribute('data-visible', false);
     mobileNavToggle.setAttribute('aria-expanded', false);
-  }
-});
-
-//Show and hide Facts list vs Hobbies list
-aboutToggle.addEventListener('click', () => {
-  const visibility = document.getElementById('facts').getAttribute('data-visible');
-
-  if (visibility === 'false') {
-    document.getElementById('facts').setAttribute('data-visible', true);
-    document.getElementById('facts').setAttribute('aria-expanded', true);
-    document.getElementById('hobbies').setAttribute('data-visible', false);
-    document.getElementById('hobbies').setAttribute('aria-expanded', false);
-    facts.style.display = 'block';
-    hobbies.style.display = 'none';
-  } else {
-    document.getElementById('hobbies').setAttribute('data-visible', true);
-    document.getElementById('hobbies').setAttribute('aria-expanded', true);
-    document.getElementById('facts').setAttribute('data-visible', false);
-    document.getElementById('facts').setAttribute('aria-expanded', false);
-    facts.style.display = 'none';
-    hobbies.style.display = 'block';
   }
 });
